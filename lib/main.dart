31c2aba459c222b06341371e39c4aa04504c662a
misterLiu <861356195@qq.com>
2020-05-12 11:15:29 +0800

    listView

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'res/listData.dart';
+
 //main 文件的基础语法
 
 void main() => runApp(MyApp());
@@ -32,69 +34,196 @@ class MyApp extends StatelessWidget {
   }  
 }
 
+// https://www.itying.com/images/flutter/1.png
 
+//
 class HomeContent extends StatelessWidget{
+  
+  List list = new List();
+  HomeContent(){
+    for (var i = 0; i < 20; i++) {
+      // this.list.add(ListTile(
+      //     title:Text('这是一个第$i个文本'),
+      //   )
+      // );
+      this.list.add('这是一个第$i个文本');
+    }
+  }
+
+  Widget _getListData(context,index){
+
+    return ListTile(
+      title: Text(listData[index]['title']),
+    );
+  }
+
+
+  List<Widget> _getData(){
+    var tempList = listData.map((value){
+       return ListTile(
+         title:Text(value['title']),
+         subtitle: Text(value['auther']),
+         leading: Image.network(value['imageurl']),
+       );
+    });
+    return tempList.toList();
+
+    // List<Widget> list = new List();
+    // for (var i = 0; i < 20; i++) {
+    //   list.add(
+    //     ListTile(
+    //       title:Text('这是一个第$i个文本')
+    //     )
+    //   );
+    // } 
+    // return list;
+
+    // return [
+    //   ListTile(
+    //     title: Text('我是一个标题'),
+    //   ),
+    //    ListTile(
+    //     title: Text('我是一个标题'),
+    //   ),
+    //    ListTile(
+    //     title: Text('我是一个标题'),
+    //   ), 
+    //   ListTile(
+    //     title: Text('我是一个标题'),
+    //   )
+    // ];
+  }
+  
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Center(
-        //Container 是个容器组件 类似div 容器组件
-        //flutter 中的所有数字 都是double 类型
-        child: Container(
-           child: Text(
-             '我是一个文本组件，显示的是文字的内容，这是一个组件，下面都是组件的属性',
-             textAlign: TextAlign.left,
-             overflow: TextOverflow.ellipsis,
-             maxLines: 2,
-             //文字行高 
-             textScaleFactor: 1.8,
-             style: TextStyle(
-               fontSize: 10.0,
-               color: Colors.cyan,
-               fontWeight: FontWeight.w800,
-               fontStyle: FontStyle.italic,
-               decoration: TextDecoration.underline,
-               decorationColor: Colors.red,
-               decorationStyle: TextDecorationStyle.dashed,
-               //字体间隔
-               letterSpacing: 15.0
+    // return Text(
+    //     '你好 flutter '
+    // );
+   
 
-             ),
-
-           ),
-           width: 300.0,
-           height: 300.0,
-           
-           decoration: BoxDecoration(
-             color: Colors.blue,
-             border: Border.all(
-               color: Colors.orange,
-               width: 2.0,
-             ),
-            //  borderRadius: BorderRadius.circular(20)
-             borderRadius: BorderRadius.all(
-               Radius.circular(10),
-                // Radius.circular(150)
-             )
-           ),
-          //  padding: EdgeInsets.all(
-          //    20
-          //  ),
-          //  padding: EdgeInsets.fromLTRB(10, 20, 30, 40),
-           margin: EdgeInsets.fromLTRB(10, 30, 5, 0),
-           transform: Matrix4.rotationZ(0.3),
-
-        ),  
-    //  child: Text(
-    //    "这是一个标题11",
-    //    textDirection: TextDirection.ltr,
-    //    style: TextStyle(
-    //      color: Colors.yellow,
-    //      fontSize: 40.0,
-    //    ) ,
-    //  ),  
+    return ListView.builder(
+          itemCount: listData.length,
+          //这个方法要注意 后面不带（）  是直接执行的意思
+          itemBuilder: this._getListData
+          /*
+          itemBuilder: (context,index){
+            // return this.list[index];
+            //  return ListTile(
+            //    title: Text(listData[index]["title"])
+            //  );
+            // return _getData()[index];
+          }
+          */
     );
+   
+
+    // return ListView.builder(
+    //       itemCount: this.list.length,
+    //       itemBuilder: (context,index){
+    //         // return this.list[index];
+    //          return ListTile(
+    //            title: Text(this.list[index])
+    //          );
+    //       }
+    // );
+
+  //  return ListView(
+  //    children: this._getData()
+  //  );
+
+
+
+    /*
+    return Container(
+      height: 180.0,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: <Widget>[
+          Container(
+            width: 180.0,
+            color: Colors.red,
+          ),
+          Container(
+            width: 180.0,
+            color: Colors.orange,
+            child: ListView(
+              // children: <Widget>[]
+              children: <Widget>[
+                 Image.network('https://www.itying.com/images/flutter/1.png'),
+                 Text('我是一个文本')
+              ],
+            )
+          )
+        ],
+      ),
+    );
+    */
+
+    /*
+    return ListView(
+      padding: EdgeInsets.all(10),
+      children: <Widget>[
+        Container(
+          width: 180.0,
+          height: 180.0,
+          color: Colors.green,
+        ),
+        Container(
+          width: 180.0,
+          height: 180.0,
+          color: Colors.red,
+        ),
+        Container(
+          width: 180.0,
+          height: 180.0,
+          color: Colors.blue,
+        ),
+        Container(
+          width: 180.0,
+          height: 180.0,
+          color: Colors.orange,
+        ),
+        Container(
+          width: 180.0,
+          height: 180.0,
+          color: Colors.cyan,
+        ),
+
+      ], 
+      */
+
+      /*  带视图的列表
+      children: <Widget>[
+        Image.network('https://www.itying.com/images/flutter/1.png'),
+        Container(
+          child: Text(
+            '我是一个标题',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 18,
+            ),
+          ),
+          height: 40,
+          padding: EdgeInsets.fromLTRB(0 , 10, 0, 10),
+        ),
+
+        Image.network('https://www.itying.com/images/flutter/1.png'),
+        Container(
+          child: Text(
+            '我是一个标题',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 18,
+            ),
+          ),
+          height: 40,
+          padding: EdgeInsets.fromLTRB(0 , 10, 0, 10),
+        ),
+       
+      ],
+      */  
   }
 }
 
-
+ 
